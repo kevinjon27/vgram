d32f8e775e0222494a251b13c530c031ad44b51f
Kevin Jonathan <kevinjonathan2701@gmail.com>
2018-07-29 16:50:46 +0700

    update analytics

--- application/modules/Home/views/footer.php
+++ application/modules/Home/views/footer.php
@@ -4,11 +4,11 @@
 		<div class="copyright book left">&copy; <?php echo date('Y') ?> <a href="https://panel.vrovent.com" target="_blank">Vrovent</a>, <?=l('copyright')?></div>
 </div>
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-106144688-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-123041204-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-106144688-1');
+  gtag('config', 'UA-123041204-1');
 </script>
