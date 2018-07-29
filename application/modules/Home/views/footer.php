bd516c40ccb6525eceecee9892e5f6bfd6dc3efa
Kevin Jonathan <kevinjonathan2701@gmail.com>
2018-07-29 16:38:22 +0700

    add analytics

--- application/modules/Home/views/footer.php
+++ application/modules/Home/views/footer.php
@@ -3,3 +3,12 @@
 	<div class="footer_content">
 		<div class="copyright book left">&copy; <?php echo date('Y') ?> <a href="https://panel.vrovent.com" target="_blank">Vrovent</a>, <?=l('copyright')?></div>
 </div>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-106144688-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-106144688-1');
+</script>
